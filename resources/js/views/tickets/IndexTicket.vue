b81be3565ec593f951131b95c137f4336afd336a
4606-raj <4606.raj@gmail.com>
2024-05-09 11:24:47 +0530

    middleware

--- resources/js/views/tickets/IndexTicket.vue
+++ resources/js/views/tickets/IndexTicket.vue
@@ -135,7 +135,26 @@
       <div class="row col-12">
 
         <div class="col-12">
-          <list :tableHeaders="tableHeaders" :tickets="tickets"/>
+          <div class="table-responsive">
+            <soft-table :headers="tableHeaders" :data="tickets"/>
+            <div class="pagination justify-content-end">
+            <soft-pagination color="primary" v-if="pagination != null">
+              <soft-pagination-item label="<" 
+              @click="changePage(pagination.current_page - 1)" 
+              :disabled="pagination.current_page === 1" />
+              
+              <soft-pagination-item v-for="page in pagination.last_page" 
+              :label="page.toString()" 
+              :class="{ active: pagination.current_page === page }" 
+              @click="changePage(page)"
+              />
+              
+              <soft-pagination-item label=">" 
+                @click="changePage(pagination.current_page + 1)" 
+                :disabled="pagination.current_page >= pagination.last_page" />
+              </soft-pagination>
+            </div>
+          </div>
         </div>
       </div>
     </div>
