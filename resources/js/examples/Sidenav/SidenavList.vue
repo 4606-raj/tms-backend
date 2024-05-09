84553181a84bf3498f6c7dd9c5acb83c27d9fc80
4606-raj <4606.raj@gmail.com>
2024-05-09 11:24:47 +0530

    middleware

--- resources/js/examples/Sidenav/SidenavList.vue
+++ resources/js/examples/Sidenav/SidenavList.vue
@@ -41,7 +41,7 @@
         </sidenav-collapse>
       </li> -->
       <li class="nav-item">
-        <sidenav-collapse navText="User Management" :to="{ name: 'Users' }" v-if="hasPermission('user_management')" >
+        <sidenav-collapse navText="User Management" :to="{ name: 'Users' }">
           <template #icon>
             <switches />
           </template>
@@ -55,7 +55,7 @@
           TEMPLATES
         </h6>
       </li> -->
-      <li class="mt-3 nav-item" v-if="hasPermission('ticket_management')">
+      <li class="mt-3 nav-item">
         <h6
           class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6"
           :class="this.$store.state.isRTL ? 'me-4' : 'ms-2'"
