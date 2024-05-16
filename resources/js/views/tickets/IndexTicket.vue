acc52ec48a1f47144eecb6e2bbf5325b233a86ac
4606-raj <4606.raj@gmail.com>
2024-05-17 00:14:57 +0530

    table-fixes

--- resources/js/views/tickets/IndexTicket.vue
+++ resources/js/views/tickets/IndexTicket.vue
@@ -135,26 +135,7 @@
       <div class="row col-12">
 
         <div class="col-12">
-          <div class="table-responsive">
-            <soft-table :headers="tableHeaders" :data="tickets"/>
-            <div class="pagination justify-content-end">
-            <soft-pagination color="primary" v-if="pagination != null">
-              <soft-pagination-item label="<" 
-              @click="changePage(pagination.current_page - 1)" 
-              :disabled="pagination.current_page === 1" />
-              
-              <soft-pagination-item v-for="page in pagination.last_page" 
-              :label="page.toString()" 
-              :class="{ active: pagination.current_page === page }" 
-              @click="changePage(page)"
-              />
-              
-              <soft-pagination-item label=">" 
-                @click="changePage(pagination.current_page + 1)" 
-                :disabled="pagination.current_page >= pagination.last_page" />
-              </soft-pagination>
-            </div>
-          </div>
+          <list :tableHeaders="tableHeaders" :tickets="tickets"/>
         </div>
       </div>
     </div>
@@ -191,7 +172,6 @@ export default {
       tableHeaders: [
         'ticketnumber', 'name(mobile)', 'email', 'mobile', 'familyid', 'category', 'subcategory', 'childsubcategory', 'Assignto', 'district', 'status', 'channel', 'loggedby', 'loggedat', 'resolveddate'
       ],
-      permissions: ['create_ticket'],
     }
   },
   async mounted() {
